0b29327ebb4554f544a4727db52772521b400443
Heytemisan <temyicy@gmail.com>
2022-01-09 23:47:05 +0100

    updated App.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function Header() {
     return (
         <div className="header">
-            Crypto
+            <header>
+                <h2 className="logo">Crypto</h2> 
+                <img src="https://cdn-icons.flaticon.com/png/512/2872/premium/2872601.png?token=exp=1641768838~hmac=86d4eeef6589132a745aecd5f998a9f9" alt="shoe" className='sneakers'/>
+            </header>       
         </div>
     )
 }
