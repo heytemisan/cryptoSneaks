db27fb8c129b92ba07e0b229dab8c966f0ee1b2b
Heytemisan <temyicy@gmail.com>
2022-01-09 23:51:08 +0100

    updated App.css

---
+++ src/components/subHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const subHeader = () => {
+    return (
+        <div>
+            <h1>Collectibe kicks for the digital age</h1>
+        </div>
+    )
+}
+
+export default subHeader
