e2fd22dd89672bafba9ce315bbcb908cffd21368
Heytemisan <temyicy@gmail.com>
2022-01-10 02:57:47 +0100

    updated SubHeader.jsx

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -3,12 +3,22 @@ import styled from 'styled-components';
 
 const Header = styled.div `
     font-family: 'IndustrialRegular'; 
+    width: 400px;
+    align-content: center;
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+
+    h1 {
+        text-align:center;
+        font-size:3rem;
+    }
 `
 
 const subHeader = () => {
     return (
         <Header>
-            Collectible kicks for the digital age
+            <h1>Collectible kicks for the digital age.</h1>
         </Header>
     )
 }
