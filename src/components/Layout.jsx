201580810b76e0fff74e67d22e985cbfef50332a
Heytemisan <temyicy@gmail.com>
2022-01-10 04:15:40 +0100

    updated SubHeader.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,6 +3,15 @@ import styled from 'styled-components';
 
 const Grid = styled.div `
     display:grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr;
+    grid-template-areas: 
+    "nav nav nav nav"
+    "sidebar main main main"
+    "sidebar content1 content2 content3"
+    "sidebar footer footer footer";
+    grid-gap: 0.4rem;
+    background-color: white;
 `
 
 const Layout = () => {
