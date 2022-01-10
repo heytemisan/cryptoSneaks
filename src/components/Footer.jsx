160111179e9481fa1dc684186a5c3115301e952e
Heytemisan <temyicy@gmail.com>
2022-01-10 13:56:00 +0100

    updated Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,14 +10,14 @@ const FooterContent = styled.div`
     margin-top:10px;
     padding-left: 10px;
     padding-left: 10px;
-    font-family: 'PixelOperatorMonoHBRegular;
+    font-family: 'PixelOperatorMonoHBRegular';
 `
 
 
 const Footer = () => {
     return (
         <FooterContent>
-            <p class="sc-gsDKAQ QEzsj">Made with ❤️ by Temy </p>
+            <h3 class="sc-gsDKAQ QEzsj">Made with ❤️ by Temisan Momodu </h3>
         </FooterContent>
     )
 }
