8cf2db25db6b161818d81790093d2614916de580
Heytemisan <temyicy@gmail.com>
2022-01-10 04:13:15 +0100

    updated SubHeader.jsx

---
+++ src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Grid = styled.div `
+    display:grid;
+`
+
+const Layout = () => {
+    return (
+        <Grid>
+            layout
+        </Grid>
+    )
+}
+
+export default Layout
